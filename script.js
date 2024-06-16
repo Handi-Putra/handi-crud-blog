4a7a15fb1ddf5b4583086599b625bfd96df08baa
Handi-Putra <43770290+Handi-Putra@users.noreply.github.com>
2024-06-16 15:40:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -257,8 +257,8 @@ async function registerUser(event, baseUrl) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      body: JSON.stringify(newUser),
     },
-    body: JSON.stringify(newUser),
   });
 
   const data = await res.json();
